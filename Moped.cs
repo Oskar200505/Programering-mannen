66b61430bee542d10963bd5133779db0f4db6f4b
Oskar200505 <oskhag050521@student.jenseneducation.se>
2022-10-21 13:10:57 +0200

    Add project files.

---
+++ Moped.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+public class moped
+{
+
+    private string motorTyp = "el";
+    private int däck = 2;
+    private string färg = "röd";
+
+    public int getDäck()
+    {
+        return this.däck;
+    }
+    public string getFärg()
+    {
+        return this.färg;
+    }
+
+    public void setFärg(string färg)
+    {
+        this.färg = färg;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Programering_mannen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            moped x = new moped();
+            x.setFärg("blå");
+            Console.WriteLine(x.getFärg());
+
+            
+           
+            
+        }
+    }
+
+}
+
+ 
+
+
+
+
+
+
+
+
+
+
+
